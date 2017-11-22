6c0a020184b5523bcb386446b8583ab2c0799e71
bixoan2101@gmail.com <PhuongDuy>
2017-11-23 02:14:48 +0700

    editProjectAgency0.6

--- PPC_Rental/Controllers/SaleController.cs
+++ PPC_Rental/Controllers/SaleController.cs
@@ -25,10 +25,15 @@ namespace PPC_Rental.Controllers
             return RedirectToAction("Index");
         }
 
-        [HttpGet]
-        public ActionResult EditProject1(int id)
+        
+        public ActionResult EditProject1(int? id)
         {
-            var editproject = db.PROPERTies.FirstOrDefault(x => x.ID == id);
+           
+            PROPERTY editproject = db.PROPERTies.Find(id);
+            if(editproject == null)
+            {
+                return HttpNotFound();
+            }
             ViewBag.editprotype = db.PROPERTY_TYPE.OrderByDescending(x => x.ID).ToList();
             ViewBag.streeid = db.STREETs.OrderByDescending(x => x.ID).ToList();
             ViewBag.disid = db.DISTRICTs.OrderByDescending(x => x.ID).ToList();
@@ -40,10 +45,11 @@ namespace PPC_Rental.Controllers
         }
 
         [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult EditProject1(int id, PROPERTY model/* string propertyname =""*/)
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProject1([Bind(Include = "ID,PropertyName,Avatar,Images,PropertyType_ID,Content,Street_ID,Ward_ID,District_ID,Price,UnitPrice,Area,BedRoom,BathRoom,PackingPlace,UserID,Created_at,Create_post,Status_ID,Note,Updated_at,Sale_ID")]PROPERTY model)
         {
-            PROPERTY pro = db.PROPERTies.FirstOrDefault(x => x.ID == id);
+
+            var pro = db.PROPERTies.Find(model.ID);
             pro.PropertyName = model.PropertyName;
             pro.Avatar = model.Avatar;
             pro.Images = model.Images;
@@ -63,23 +69,15 @@ namespace PPC_Rental.Controllers
             pro.Create_post = model.Create_post;
             pro.Status_ID = model.Status_ID;
             pro.Note = model.Note;
-            pro.Updated_at = model.Updated_at;
+            pro.Updated_at = DateTime.Now;
             pro.Sale_ID = model.Sale_ID;
             pro.PROPERTY_FEATURE = model.PROPERTY_FEATURE;
 
-
-            //db.Entry(model).State = System.Data.Entity.EntityState.Modified;
-            //try
-            //{
-            db.PROPERTies.Add(pro);
+            db.Entry(pro).State = System.Data.Entity.EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
-            //}
-            //catch (Exception e)
-            //{
-            //    ViewBag.Error = e.Message;
-            //    return View(model);
-            //}
+            
+           
 
         }
 
@@ -87,6 +85,52 @@ namespace PPC_Rental.Controllers
         {
             var editproject = db.PROPERTies.FirstOrDefault(x => x.ID == id);
             return View(editproject);
+        }
+
+        [HttpGet]
+        public ActionResult AddProject()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AddProject(PROPERTY model)
+        {
+            var pro = new PROPERTY();
+            pro.PropertyName = model.PropertyName;
+            pro.Avatar = model.Avatar;
+            pro.Images = model.Images;
+            pro.PropertyType_ID = model.PropertyType_ID;
+            pro.Content = model.Content;
+            pro.Street_ID = model.Street_ID;
+            pro.Ward_ID = model.Ward_ID;
+            pro.District_ID = model.District_ID;
+            pro.Price = model.Price;
+            pro.UnitPrice = model.UnitPrice;
+            pro.Area = model.Area;
+            pro.BedRoom = model.BedRoom;
+            pro.BathRoom = model.BathRoom;
+            pro.PackingPlace = model.PackingPlace;
+            pro.UserID = model.UserID;
+            pro.Created_at = DateTime.Now;
+            pro.Create_post = DateTime.Now;
+            pro.Status_ID = model.Status_ID;
+            pro.Note = model.Note;
+            pro.Updated_at = DateTime.Now;
+            pro.Sale_ID = model.Sale_ID;
+            pro.PROPERTY_FEATURE = model.PROPERTY_FEATURE;
+            db.PROPERTies.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public JsonResult GetStreet(int District_id)
+        {
+            return Json(
+            db.STREETs.Where(s => s.District_ID == District_id)
+            .Select(s => new { id = s.ID, text = s.StreetName }).ToList(),
+            JsonRequestBehavior.AllowGet);
         }
     }
 }
